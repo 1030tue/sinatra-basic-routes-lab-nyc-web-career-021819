21e0f5bab86e3ff81f839363e3fbdd0484a26ea4
Kate Kim <katekim5809@gmail.com>
2019-03-11 02:58:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Started my app using shotgun!!!!"
+end
+
+  get '/name' do
+    "My name is Kate"
+  end
+
+  get '/hometown' do
+    "My hometown is Korea"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Beatles"
+  end
+
+
 end
